c5c428f8badf4dd5c665d6683207e01d2d617b00
Bouke Stam <bouke.stam@gmail.com>
2023-07-25 21:39:27 +0200

    Remove cookie stuff

--- src/browse.ts
+++ src/browse.ts
@@ -17,30 +17,16 @@ export async function browse(url: string): Promise<string> {
     ]
   });
 
-  const page = await browser.newPage();
+  try {
+    const page = await browser.newPage();
 
-  await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 10000 });
+    await page.goto(url, { waitUntil: 'networkidle0', timeout: 10000 });
 
-  await page.evaluate(() => {
-    function xcc_contains(selector: string, text: string) {
-      const elements = [...document.querySelectorAll(selector)];
-      return elements.filter(element => {
-        return RegExp(text, "i").test(element.textContent.trim());
-      });
-    }
-    var _xcc;
-    _xcc = xcc_contains('button, a', '^(Accept all|Accept|I understand|Agree|Okay|OK)$');
-    if (_xcc != null && _xcc.length != 0) {
-      console.log("Found");
-      (_xcc[0] as HTMLElement).click();
-    }
-  });
-
-  await page.waitForSelector('body');
-
-  const data = await page.evaluate(() => document.querySelector('*').outerHTML);
-
-  await browser.close();
-
-  return data;
+    const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+    return data;
+  } catch (e) {
+    throw e;
+  } finally {
+    await browser.close();
+  }
 }
